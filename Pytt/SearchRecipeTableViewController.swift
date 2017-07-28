c3dbb49a0f7a3cfe9fe730db0ed98b8711c85083
Unknown <johan.hosk@gmail.com>
2017-07-28 14:53:43 +0200

    Added tutorial link

---
+++ Pytt/SearchRecipeTableViewController.swift
@@ -0,0 +1,11 @@
+//
+//  SearchRecipeTableViewController.swift
+//  Pytt
+//
+//  Created by Hosk, Johan on 2017-07-28.
+//  Copyright © 2017 Wirecard. All rights reserved.
+//
+
+//Check here for basic pattern https://stackoverflow.com/documentation/ios/9467/mvp-architecture#t=201707281252273684482
+
+import Foundation
